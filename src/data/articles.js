5f0c4217edd873150d11673c5bf30e7c5193172c
NuwanKeshara <0712495822Aa#>
2025-09-08 23:34:03 +0300

    remove keywords

--- src/data/articles.js
+++ src/data/articles.js
@@ -8,9 +8,6 @@ function article_1() {
 			"Cloud computing offers a range of benefits, including cost savings and increased flexibility. Find out why more businesses are turning to the cloud.",
 		keywords: [
 			"The Benefits of Cloud Computing",
-			"Nuwan",
-			"Nuwan K",
-			"Nuwan Keshara",
 		],
 		style: `
 				.article-content {
